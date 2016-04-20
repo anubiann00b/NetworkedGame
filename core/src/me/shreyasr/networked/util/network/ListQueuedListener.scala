251074b02c4544b9bc4e70a9d0c05964cc2af1b9
Shreyas <skraman1999@gmail.com>
2016-04-20 00:19:02 -0700

    Fix QueuedListener concurrency issue.

--- core/src/me/shreyasr/networked/util/network/ListQueuedListener.scala
+++ core/src/me/shreyasr/networked/util/network/ListQueuedListener.scala
@@ -8,11 +8,20 @@ import scala.collection.mutable
 class ListQueuedListener(listener: Listener = null) extends QueuedListener(listener) {
 
   val list = new mutable.MutableList[Runnable]
+  val temp = new mutable.MutableList[Runnable]
 
-  override protected def queue(runnable: Runnable): Unit = list += runnable
+  override protected def queue(runnable: Runnable): Unit = {
+    temp.synchronized {
+      temp += runnable
+    }
+  }
 
   def run() = {
     list.foreach(_.run)
     list.clear
+    temp.synchronized {
+      list ++= temp
+      temp.clear()
+    }
   }
 }
